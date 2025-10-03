0840dca7fabd1dfc9b93ea96d355a04287168616
Vaggelis <roudosisevang@gmail.com>
2025-10-04 00:05:42 +0300

    Add files via upload

---
+++ Switch Themes/main.js
@@ -0,0 +1,24 @@
+
+ function changeLightTheme (){
+  document.querySelector("body").style.backgroundColor ="#f0f8ff";
+  document.querySelector("h1").style.color ="black";
+  document.querySelector("p").style.color ="black";
+  document.querySelector("button").style.backgroundColor = "black";
+  document.querySelector("button").style.color = "white";
+
+}
+
+function changeTheme(){
+  document.querySelector("body").style.backgroundColor ="rgb(28, 28, 30)";
+  document.querySelector("h1").style.color ="white";
+  document.querySelector("p").style.color ="white";
+  document.querySelector("button").style.backgroundColor = "black";
+  document.querySelector("button").style.color = "white";
+
+} 
+
+
+
+
+
+  
